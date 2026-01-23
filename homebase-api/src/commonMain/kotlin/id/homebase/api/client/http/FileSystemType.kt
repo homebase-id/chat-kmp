919b9c1aa218e0c829ecf1d1917977fc82738aac
toddmitchell <t@lighttheory.co>
2026-01-23 14:23:45 -0800

    minor tweaks

--- homebase-api/src/commonMain/kotlin/id/homebase/api/client/http/FileSystemType.kt
+++
@@ -1,9 +0,0 @@
-package id.homebase.homebasekmppoc.prototype.lib.http
-
-
-enum class FileSystemType(val value: String) {
-    Standard("128"),
-    Comment("32");
-
-    override fun toString() = value
-}
