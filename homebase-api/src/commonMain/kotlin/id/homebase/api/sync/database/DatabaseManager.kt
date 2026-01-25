c1a780e6ea60bab36a827a9c84828f314231b3f7
Seifert69 <michael@corleone.com>
2026-01-25 15:55:03 +0100

    Catching up, now need to add tests

--- homebase-api/src/commonMain/kotlin/id/homebase/api/sync/database/DatabaseManager.kt
+++ homebase-api/src/commonMain/kotlin/id/homebase/api/sync/database/DatabaseManager.kt
@@ -14,6 +14,9 @@ private val appNotificationsAdapter = AppNotifications.Adapter(
     identityIdAdapter = UuidAdapter,
     notificationIdAdapter = UuidAdapter
 )
+private val chatReadCountAdapter = ChatReadCount.Adapter(
+    groupIdAdapter = UuidAdapter
+)
 private val driveMainIndexAdapter = DriveMainIndex.Adapter(
     identityIdAdapter = UuidAdapter,
     driveIdAdapter = UuidAdapter,
@@ -57,6 +60,7 @@ class DatabaseManager(driverProvider: () -> SqlDriver) : AutoCloseable {
         database = OdinDatabase(
             driver,
             appNotificationsAdapter,
+            chatReadCountAdapter,
             driveLocalTagIndexAdapter,
             driveMainIndexAdapter,
             driveTagIndexAdapter,
