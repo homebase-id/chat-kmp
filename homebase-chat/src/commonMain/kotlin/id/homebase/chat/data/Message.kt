642d035c196b064ba898c57b27933fe10c622d21
Seifert69 <michael@corleone.com>
2026-01-27 02:26:29 +0100

    pip

--- homebase-chat/src/commonMain/kotlin/id/homebase/chat/data/Message.kt
+++ homebase-chat/src/commonMain/kotlin/id/homebase/chat/data/Message.kt
@@ -7,15 +7,15 @@ import kotlin.uuid.Uuid
 
 @Immutable
 data class Message(
-    val id: Uuid,
-    val conversationId: Uuid,
-    val content: String,
-    val timestamp: Instant,
-    val senderId: String,
-    val senderOdinId: String,
-    val isCurrentUser: Boolean = false,
+    val id: Uuid, // uniqueId
+    val conversationId: Uuid, // groupId
+    val content: String, // the message
+    val timestamp: Instant, // When the message was sent
+    val senderId: String, // TODO: What is that? The name?
+    val senderOdinId: String, // frodo.baggins.demo.rocks
+    val isCurrentUser: Boolean = false, // TODO: What is that?
     val isRead: Boolean = false,
-    val messageAppData: MessageAppData
+    val messageAppData: MessageAppData // TODO: Should we copy these up into the message?
 ) {
     companion object {
         /**
