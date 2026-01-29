e83a0606f6de80261ee440e1d02caa50d41fb207
Anders Carlsen <acarlsen@gmail.com>
2026-01-29 22:30:05 +0100

    Init storage/preferences on Android

--- androidApp/src/main/kotlin/id/homebase/android/MainApplication.kt
+++ androidApp/src/main/kotlin/id/homebase/android/MainApplication.kt
@@ -1,7 +1,10 @@
 package id.homebase.android
 
 import android.app.Application
+import id.homebase.api.storage.SecureStorage
+import id.homebase.api.storage.SharedPreferences
 import id.homebase.core.di.allModules
+import kotlinx.coroutines.runBlocking
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.component.KoinComponent
@@ -10,6 +13,15 @@ import org.koin.core.context.GlobalContext.startKoin
 class MainApplication : Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
+
+        // Initialize storage (must be done before App() which may access storage)
+        SecureStorage.initialize(this)
+        SharedPreferences.initialize(this) // TODO: Maybe we should use injectable UserPreferences
+
+        runBlocking {
+            // DatabaseManager.wipe { DatabaseDriverFactory(applicationContext).createDriver() } // <-- Uncomment to wipe database
+            // DatabaseManager.initialize { DatabaseDriverFactory(this).createDriver() } // TODO: This is not working
+        }
 
         startKoin {
             // Log Koin into Android logger
