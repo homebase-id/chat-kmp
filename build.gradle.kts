822b95ac7cfcb3252f3305f713550ba02f7e56d9
Anders Carlsen <acarlsen@gmail.com>
2026-02-09 18:17:47 +0100

    Added detekt and ktlint - many issues detected currently, detekt does not run with AGP 9.0.0 yet
    # Conflicts:
    #	build.gradle.kts
    #	gradle/libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,38 @@ plugins {
     alias(libs.plugins.composeHotReload) apply false
     alias(libs.plugins.androidLint) apply false
     alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.ktlint.rules)
+    }
+}
+
+subprojects {
+    // Enable detekt again when supporting new format
+    // https://github.com/detekt/detekt/issues/8981
+//    apply(plugin = "dev.detekt")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+//    configure<dev.detekt.gradle.extensions.DetektExtension> {
+//        buildUponDefaultConfig = true
+//        allRules = false
+//        config.setFrom("$rootDir/config/detekt.yml")
+//    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+//        android.set(true)
+//        outputColorName.set("RED")
+
+        filter {
+            //exclude("**/generated/**", "**/commonTest/**", "**/androidTest/**")
+            exclude { element -> element.file.path.contains("generated/") }
+            exclude { element -> element.file.path.contains("/commonTest/") }
+            exclude { element -> element.file.path.contains("/androidTest/") }
+            //include("**/kotlin/**")
+        }
+    }
 }
