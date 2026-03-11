85dcd5960b4911bdbf052f44dd1540dadeb5db0c
toddmitchell <t@lighttheory.co>
2026-03-10 19:59:45 -0500

    grrr serial

--- homebase-chat/src/commonMain/kotlin/id/homebase/chat/services/StatusMessageData.kt
+++ homebase-chat/src/commonMain/kotlin/id/homebase/chat/services/StatusMessageData.kt
@@ -1,7 +1,9 @@
 package id.homebase.chat.services
 
 import id.homebase.api.common.OdinId
+import kotlinx.serialization.Serializable
 
+@Serializable
 class StatusMessageData(
     val statusMessage: StatusMessage,
     val subject: OdinId? = null
