27b48e904926da90a9214fe40056fb48985d247d
toddmitchell <t@lighttheory.co>
2026-03-20 14:49:55 -0500

    fixes

--- homebase-api/src/commonMain/kotlin/id/homebase/api/video/VideoMetadata.kt
+++ homebase-api/src/commonMain/kotlin/id/homebase/api/video/VideoMetadata.kt
@@ -1,15 +1,17 @@
 package id.homebase.api.video
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
 data class VideoMetadata(
     val mimeType: String,
     val isDescriptorContentComplete: Boolean = true,
     val isSegmented: Boolean,
     val fileSize: Long,
-    val durationMs: Long,
-    val key: String,
+    val duration: Float = 0F,
+    val key: String = "",
     val codec: String,
     val hlsPlaylist: String? = null,
 )
