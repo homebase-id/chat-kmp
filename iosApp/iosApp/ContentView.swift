3e5176b1a25515f522ea6932a2db9405fb193027
Anders Carlsen <acarlsen@gmail.com>
2026-04-09 13:04:20 +0200

    Potentially handle crash in iOS app when run on macOS via Mac Catalyst

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -8,6 +8,14 @@ struct ComposeView: UIViewControllerRepresentable {
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+
+    static func dismantleUIViewController(_ uiViewController: UIViewController, coordinator: ()) {
+        // Clear the reference to allow proper cleanup
+        if let currentInstance = MainViewControllerRef.shared.instance,
+           currentInstance === uiViewController {
+            MainViewControllerRef.shared.instance = nil
+        }
+    }
 }
 
 struct ContentView: View {
