d3ee6425420c4f2dea9eacc67759c891b8145b28
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2026-04-13 16:44:08 +0530

    Revert "fix fix fix (#271)"
    This reverts commit ed47c0ca920e01379779adabdb664e37f72e4ccf.

--- homebase-chat/src/nativeMain/kotlin/id/homebase/chat/widget/video/LocalVideoPlayerSurface.native.kt
+++ homebase-chat/src/nativeMain/kotlin/id/homebase/chat/widget/video/LocalVideoPlayerSurface.native.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.UIKitViewController
+import androidx.compose.ui.interop.UIKitViewController
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.AVFoundation.AVPlayer
 import platform.AVFoundation.AVPlayerItemDidPlayToEndTimeNotification
